57a80a06d6946bf5c49d342087805e12ec4b78b2
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-08-11 11:50:14 +0530

    new test cases

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ var ij = require('../index');
 
 var obj1 = { city: 'Chennai', state: 'Tamil Nadu'};
 var obj1_result = [{ city: 'Chennai', state: 'Tamil Nadu', _index: 'chennaitamilnadu'}];
+
 var obj2 = [
 	{city: 'Chennai', state: 'Tamil Nadu'},
 	{city: 'Mumbai', state: 'Maharashtra'}
@@ -13,8 +14,17 @@ var obj2_result = [
 	{city: 'Chennai', state: 'Tamil Nadu', _index: 'chennaitamilnadu'},
 	{city: 'Mumbai', state: 'Maharashtra', _index: 'mumbaimaharashtra'}
 ];
+
 var obj3 = { city: 'Chennai', dist: 'Chennai'};
 var obj3_result = [{ city: 'Chennai', dist: 'Chennai', _index: 'chennai'}];
+
+var obj4 = { city: 'Chennai', state: 'NA'};
+var obj4_result = [{ city: 'Chennai', state: 'NA', _index: 'chennaina'}];
+var obj4_result_skip = [{ city: 'Chennai', state: 'NA', _index: 'chennai'}];
+
+var obj5 = { city: 'Chennai', state: { name: 'Tamil Nadu'}};
+var obj5_result = [{city: 'Chennai', state: { name: 'Tamil Nadu'}, _index: 'chennai'}];
+
 var keys = ['city', 'state'];
 
 describe('Index JSON', function() {
@@ -51,4 +61,33 @@ describe('Index JSON', function() {
 		var result = ij.index(obj3, keys);
 		expect(result).to.deep.equal(obj3_result);
 	});
+
+	it('should index only the available keys even if skipValue is given', function() {
+		var result = ij.index(obj3, keys, 'na');
+		expect(result).to.deep.equal(obj3_result);
+	});
+
+	it('should throw "skipValue should be a string" error', function() {
+		expect(ij.index.bind(null, obj4, keys, 1)).to.throw(Error, 'skipValue should be a string');
+	});
+
+	it('should not include skipValue in index', function() {
+		var result = ij.index(obj4, keys, 'NA');
+		expect(result).to.deep.equal(obj4_result_skip);
+	});
+
+	it('should index even if skipValue is not given', function() {
+		var result = ij.index(obj4, keys);
+		expect(result).to.deep.equal(obj4_result);
+	});
+
+	it('should index if skipValue is not equal to any value', function() {
+		var result = ij.index(obj4, keys, 'lol');
+		expect(result).to.deep.equal(obj4_result);
+	});
+
+	it('should skip indexing object values', function() {
+		var result = ij.index(obj5, keys);
+		expect(result).to.deep.equal(obj5_result);
+	})
 });
